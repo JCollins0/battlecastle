525e58114ddf15282e8f46686619b66fa3ef90ed
s341084 <s341084@003L2046-645966.cfisd.loc>
2015-11-10 12:04:41 -0600

    Physics Engine wooooo

---
+++ BattleCastle/src/game/physics/Edge.java
@@ -0,0 +1,28 @@
+package game.physics;
+
+public class Edge
+{
+	
+	private Vector max;
+	private Vector edge;
+	
+	public Edge()
+	{
+		
+	}
+
+	public Vector getMax() {
+		return max;
+	}
+
+	public Vector getEdge() {
+		return edge;
+	}
+	
+	public void set(Vector max,Vector edge)
+	{
+		this.max=max;
+		this.edge=edge;
+	}
+
+}
